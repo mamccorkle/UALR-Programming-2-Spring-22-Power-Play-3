79a79e4ccd76be4b42bd94fdda2c79733e5f245f
mamccorkle <89654757+mamccorkle@users.noreply.github.com>
2022-02-04 16:37:58 -0600

    Add project files.

---
+++ Monster Attack Unit Test/Monster Attack Unit Test.cpp
@@ -0,0 +1,28 @@
+//
+//  Monster Attack Unit Test.cpp
+//
+//  Project: UALR - Programming 2 - Spring 22 - Power Play 3 - Unit Tests
+//  Created by: Mark McCorkle on 20220204
+//  Based on: Power Play 2 by Sean Orme
+//  IDE: CLion 2021.2.3     - VERIFIED/UNVERIFIED
+//  IDE: XCode              - VERIFIED/UNVERIFIED
+//  IDE: Visual Studio 2019 - VERIFIED
+//  IDE: Visual Studio 2022 - VERIFIED/UNVERIFIED
+//  IDE: Linux via g++      - VERIFIED/UNVERIFIED
+//
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MonsterAttackUnitTest
+{
+	TEST_CLASS(MonsterAttackUnitTest)
+	{
+	public:
+
+		TEST_METHOD(MonsterAttackTestMethod)
+		{
+		}
+	};
+}
